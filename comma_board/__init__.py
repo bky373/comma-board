07e20eaff7e62855106c4d8924c49c30c1c406d1
bky373 <bo373@naver.com>
2021-02-12 20:20:55 +0900

    feat(common) : enable CSRF Protection globally

--- comma_board/__init__.py
+++ comma_board/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 
 import config
 
 db = SQLAlchemy()
 migrate = Migrate()
+
+csrf = CSRFProtect()
 
 
 def register_blueprints(app):
@@ -20,6 +23,8 @@ def register_blueprints(app):
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+
+    csrf.init_app(app)
 
     db.init_app(app)
     migrate.init_app(app, db)
